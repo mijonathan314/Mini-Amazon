5d765e4148b8108d47874ce725462bddabe5217b
Jonathan <jfm49@duke.edu>
2023-10-05 01:48:20 -0400

    Finished Milestone 3

--- app/product.py
+++ app/product.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask_login import current_user
 import datetime
-from flask import jsonify
+from flask import jsonify, request
 
 from .models.product import Product
 
@@ -9,11 +9,16 @@ from flask import Blueprint
 bp = Blueprint('product', __name__)
 
 
-@bp.route('/product/<int:k>', methods=['GET'])
-def get_top_products(k):
-    # find the top k most expensive products
-    if current_user.is_authenticated:
-        product = Product.get_top_expensive_products(k)
+@bp.route('/product', methods=['POST'])
+def get_top_products():
+    if request.method == 'POST':
+        k = int(request.form.get('k-value', 1))  # Get the value of k from the form
     else:
-        product = None
-    return render_template('products.html', top_product = product)
+        k = 0  # Default value if no input provided
+
+    if current_user.is_authenticated:
+        products = Product.get_top_expensive_products(k)
+    else:
+        products = None
+
+    return render_template('products.html', top_product=products)
