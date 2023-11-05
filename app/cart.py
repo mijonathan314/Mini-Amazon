4bfcbf4a94084f21ee0ed5fa75e138e80037e520
Naomie Gao <ng178@duke.edu>
2023-11-04 20:54:11 -0400

    pagination

--- app/cart.py
+++ app/cart.py
@@ -12,14 +12,24 @@ bp = Blueprint('cart', __name__)
 
 @bp.route('/cart', methods=['GET'])
 def cart():
-    # get all items in user's cart
     if current_user.is_authenticated:
-        cart_items = Cart.get_cart_items(current_user.id)
+        # get all items in the cart
+        all_cart_items = Cart.get_cart_items(current_user.id)
+        page = int(request.args.get('page', 1))
+        items_per_page = 10
+        start_idx = (page - 1) * items_per_page
+        end_idx = page * items_per_page
+        # get the items on the current page
+        cart_items = all_cart_items[start_idx:end_idx]
+        total_pages = (len(all_cart_items) + items_per_page - 1) // items_per_page
+
     else:
         cart_items = None
+        page = None
+        total_pages = None
 
     return render_template('cart.html',
-                           cart_items=cart_items)
+                           cart_items=cart_items, page=page, total_pages=total_pages)
 
 @bp.route('/update-cart-item/<int:pid>', methods=['PATCH'])
 def update_cart_item(pid):
