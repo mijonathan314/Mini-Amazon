868f02df17b217dce94d97b482e84114bc51768d
Naomie Gao <ng178@duke.edu>
2023-12-09 17:51:55 -0500

    fix

--- app/inventory.py
+++ app/inventory.py
@@ -65,6 +65,8 @@ def seller(action = None, user_id = None, product_id = None, order_id = None, bu
             UPDATE Purchases
             SET fulfillment_status = :fulfillment_status, time_fulfillment_updated=:now
             WHERE pid = :product_id
+            AND uid =:buyer_id
+            AND order_id =:order_id
             ''', fulfillment_status = request.form['quant'], order_id = order_id, product_id = product_id, buyer_id=buyer_id, now=now)
             return redirect(url_for('inventory.seller'))
 
--- app/models/seller.py
+++ app/models/seller.py
@@ -85,9 +85,11 @@ class Fulfillment:
 SELECT Orders.user_id, Orders.id, Users.address, Products.name, Purchases.fulfillment_status, Orders.time_stamp, Orders.total_items, Products.id
 FROM Orders, Products, Purchases, Users
 WHERE (
-    Products.user_id = Users.id AND
+    Products.user_id = :user_id AND
     Products.id = Purchases.pid AND
-    Users.id = Orders.user_id
+    Purchases.uid = Users.id AND
+    Orders.id = Purchases.order_id
+
 )
 ORDER BY Orders.time_stamp DESC
 ''',
