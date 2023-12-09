492e749c5205f37f33c68d9b541ccef93def35f2
Tad <tsk17@duke.edu>
2023-12-09 13:34:31 -0500

    add pagination and chart for order history

--- app/purchases.py
+++ app/purchases.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import current_user
 import datetime
 
@@ -15,7 +15,15 @@ def get_orders_by_uid(uid):
     orders = None
     if current_user.is_authenticated:
         orders = Order.get_by_uid(uid)
-    return render_template('orderHistory.html', order_history=orders)
+        page = int(request.args.get('page', 1))
+        items_per_page = 8
+        start_idx = (page - 1) * items_per_page
+        end_idx = page * items_per_page
+        # get the items on the current page
+        order_list = orders[start_idx:end_idx]
+        total_pages = (len(orders) + items_per_page - 1) // items_per_page
+    return render_template('orderHistory.html', order_history=order_list, orders=orders,
+                           page=page, total_pages=total_pages)
 
 @bp.route('/purchases/<int:uid>', methods=['GET'])
 def get_purchases_by_uid(uid):
@@ -24,8 +32,17 @@ def get_purchases_by_uid(uid):
     category_counts = {}
     if current_user.is_authenticated:
         purchases = Purchase.get_all_by_uid(uid)
+        page = int(request.args.get('page', 1))
+        items_per_page = 8
+        start_idx = (page - 1) * items_per_page
+        end_idx = page * items_per_page
+        # get the items on the current page
+        purchase_list = purchases[start_idx:end_idx]
+        total_pages = (len(purchases) + items_per_page - 1) // items_per_page
+
         for purchase in purchases: 
             if purchase.category not in category_counts: 
                 category_counts[purchase.category] = 0
             category_counts[purchase.category] += 1
-    return render_template('purchases.html', purchase_history=purchases, category_counts=category_counts)
+    return render_template('purchases.html', purchase_history=purchase_list, page=page, 
+                           total_pages=total_pages, category_counts=category_counts)
